4a905820b3d6679d73e9baeb097b1eeb29c86ff6
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-03-18 01:12:37 +0100

    Added tests to node-html

---
+++ node-html-form/routes/__tests__/home.test.js
@@ -0,0 +1,17 @@
+const request = require("supertest");
+const app = require("../../src/app");
+
+describe("homeRouter tests", () => {
+  test("root endpoint should respond with status OK", async () => {
+    const response = await request(app).get("/");
+
+    expect(response.status).toBe(200);
+  });
+
+  test("root endpoint returns some HTML", async () => {
+    const response = await request(app).get("/");
+
+    expect(response).not.toBeNull();
+    expect(response.text).toMatch(/Newsletter/i);
+  });
+});
