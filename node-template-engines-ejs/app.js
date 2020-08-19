1317569d6fafc2b2c8f42b7fa3e3d76ad4f8b2e0
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-08-19 13:28:50 +0200

    :see_no_evil: Hid .env file

--- node-template-engines-ejs/app.js
+++ node-template-engines-ejs/app.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 3000;
 
 // Routes import
 const homeRouter = require("./routes/homeRouter");
