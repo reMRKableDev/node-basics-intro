144181cf5b8a4d96e81c7cbdd992103a6f9c596c
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-08-24 14:34:11 +0200

    :sparkles: New views added

--- node-library-crud-mongo-express/routes/index.js
+++ node-library-crud-mongo-express/routes/index.js
@@ -1,27 +1,9 @@
-const express = require("express");
-const router = express.Router();
-
-const Book = require("../models/Book.model");
+const express = require('express');
+const router  = express.Router();
 
 /* GET home page */
-router.get("/", (_, res) => {
-  Book.find()
-    .then((allTheBooksFromDB) =>
-      res.render("books-list", { allTheBooksFromDB })
-    )
-    .catch((error) =>
-      console.log("Error while getting the books from the DB: ", error)
-    );
-});
-
-router.get("/:bookId", (req, res) => {
-  const { bookId } = req.params;
-
-  Book.findOne({ _id: bookId })
-    .then((theBook) => res.render("books-details", theBook))
-    .catch((error) =>
-      console.log("Error while retrieving book details: ", error)
-    );
+router.get('/', (req, res, next) => {
+  res.render('index');
 });
 
 module.exports = router;
