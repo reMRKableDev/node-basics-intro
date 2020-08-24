19baf967cfd5c711138647cf4e7327a9e3ab6584
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-08-24 14:31:14 +0200

    :sparkles: New routes for index.js

--- node-library-crud-mongo-express/routes/index.js
+++ node-library-crud-mongo-express/routes/index.js
@@ -1,9 +1,27 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const Book = require("../models/Book.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (_, res) => {
+  Book.find()
+    .then((allTheBooksFromDB) =>
+      res.render("books-list", { allTheBooksFromDB })
+    )
+    .catch((error) =>
+      console.log("Error while getting the books from the DB: ", error)
+    );
+});
+
+router.get("/:bookId", (req, res) => {
+  const { bookId } = req.params;
+
+  Book.findOne({ _id: bookId })
+    .then((theBook) => res.render("books-details", theBook))
+    .catch((error) =>
+      console.log("Error while retrieving book details: ", error)
+    );
 });
 
 module.exports = router;
