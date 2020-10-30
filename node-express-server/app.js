ca1610d86c24943433c33347ed99c881068346f6
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-10-30 16:08:30 +0100

    :heavy_plus_sign: Added morgan logger to workflow

--- node-express-server/app.js
+++ node-express-server/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
