0343b73ed8b0abf8c7e5481e38a79b4475602d99
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-10-30 12:13:41 +0100

    :fire: Removed jsdocs from folder for more simplicity

--- node-http-server/app.js
+++ node-http-server/app.js
@@ -1,30 +1,12 @@
-/**
- * @const http                              Import http dependency
- * @requires module:http
- */
-
 const http = require("http");
-
-/**
- * @const app                               Application container that holds server instructions
- * @method http.createServer                Returns a new instance of TPC/IPC server object
- * 
- * @function requestListener                Handles the requests and response from and to the user
- * @param {request} IncomingMessage         Represents the request to the server         
- * @param {response} ServerResponse         Represents a writable stream back to the client
- * 
- * @method request.url                      Represents URL endpoint for the request
- * @method response.writeHead               Sends status and response headers to the client
- * @method response.write()                 Sends text, or a text stream, to the client
- * @method response.end()                   Signals that the server should consider the response finished
- */
 
 const app = http.createServer((request, response) => {
   if (request.url === "/") {
     response.writeHead(200);
     response.end("World");
   }
-  if (request.url === "/goodbye") {
+
+  if (request.url === "/about") {
     response.writeHead(200);
     response.write("Goodbye World");
     response.end();
--- node-http-server/server.js
+++ node-http-server/server.js
@@ -1,18 +1,4 @@
-/**
- * @const app                   Import module with server instructions
- * @requires module:app
- * 
- * @const port                  Container for the port value for the server
- * 
- */
 const app = require("./app");
 const port = 3000;
 
-/**
- * @method app.listen           Create a listener on the specified port 
- * 
- * @param port                  Specifies the port we want to listen to
- * @param callbackFunction      Specifies a function to be executed when the listener is added         
- * 
- */
 app.listen(port, () => console.log(`Server is running on port ${port}`));
