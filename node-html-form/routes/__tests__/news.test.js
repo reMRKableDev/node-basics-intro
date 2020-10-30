670d5903584e0eabbafeab21c08a0d0674dfa268
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-10-30 21:32:27 +0100

    :white_check_mark: Introduced global variable for reusable code

--- node-html-form/routes/__tests__/news.test.js
+++ node-html-form/routes/__tests__/news.test.js
@@ -2,15 +2,17 @@ const request = require("supertest");
 const app = require("../../src/app");
 
 describe("newsRouter tests", () => {
+  const getNewsletterRequest = request(app).get("/newsletter");
+
   test("root GET route to return with status OK", async () => {
-    const response = await request(app).get("/newsletter");
+    const response = await getNewsletterRequest;
     const { statusCode } = response;
 
     expect(statusCode).toBe(200);
   });
 
   test("root GET route to return some HTML", async () => {
-    const response = await request(app).get("/newsletter");
+    const response = await getNewsletterRequest;
     const { text } = response;
 
     expect(response).not.toBeNull();
