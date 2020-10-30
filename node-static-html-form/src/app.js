2ebd784ebecafac35bcc85add97e167417185289
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-10-30 22:02:44 +0100

    :heavy_plus_sign: Installed morgan as logger

--- node-static-html-form/src/app.js
+++ node-static-html-form/src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use("/", require("../routes/home.route"));
 app.use("/newsletter", require("../routes/news.route"));
