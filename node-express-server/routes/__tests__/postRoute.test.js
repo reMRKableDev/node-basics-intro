22f6fa7b32016b21b5d3355c16dac793a743ea94
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-11-01 01:57:50 +0100

    :white_check_mark: Added new tests and introduced helpers

--- node-express-server/routes/__tests__/postRoute.test.js
+++ node-express-server/routes/__tests__/postRoute.test.js
@@ -1,41 +1,56 @@
 const request = require("supertest");
 const app = require("../../src/app");
 
-describe("postRouter tests", () => {
-  const testPosts = [
-    { id: 1, title: "first title", content: "first content" },
-    { id: 2, title: "second title", content: "second content" },
-    { id: 3, title: "third title", content: "third content" },
-  ];
+const getPostsRequest = () => request(app).get("/posts");
+const getPostFromQueryParam = (param) => request(app).get(`/posts/${param}`);
+const postNewPostRequest = (obj) => request(app).post("/posts/new").send(obj);
 
-  test("GET endpoint returns status OK", async () => {
-    const response = await request(app).get("/posts");
+const testPosts = [
+  { id: 1, title: "first title", content: "first content" },
+  { id: 2, title: "second title", content: "second content" },
+  { id: 3, title: "third title", content: "third content" },
+];
+const dummyData = { data: "dummyData" };
+
+describe("Test suite for '/posts' endpoint", () => {
+  test("should validate GET for '/posts' endpoint returns status code 200", async () => {
+    const response = await getPostsRequest();
     const { statusCode } = response;
 
+    expect(statusCode).not.toBe(404);
     expect(statusCode).toBe(200);
   });
 
-  test("GET endpoint returns valid data", async () => {
-    const response = await request(app).get("/posts");
+  test("should validate GET for '/posts' endpoint returns valid data", async () => {
+    const response = await getPostsRequest();
     const { body } = response;
 
+    expect(body).not.toHaveLength(100000);
     expect(body).toHaveLength(3);
+
+    expect(body).not.toEqual(expect.arrayContaining([dummyData]));
     expect(body).toEqual(expect.arrayContaining(testPosts));
   });
 
-  test("GET endpoint with valid query params", async () => {
-    const response = await request(app).get("/posts/1");
+  test("should validate GET endpoint with valid query params", async () => {
+    const response = await getPostFromQueryParam("1");
     const { statusCode, body } = response;
 
+    expect(statusCode).not.toBe(404);
     expect(statusCode).toBe(200);
+
+    expect(body).not.toEqual(testPosts[2]);
     expect(body).toEqual(testPosts[0]);
   });
 
   test("GET endpoint with invalid query params", async () => {
-    const response = await request(app).get("/posts/10");
+    const response = await getPostFromQueryParam("10");
     const { statusCode, body } = response;
 
+    expect(statusCode).not.toBe(304);
     expect(statusCode).toBe(400);
+
+    expect(body).not.toBeNull();
     expect(body).toHaveProperty("error");
   });
 
@@ -46,19 +61,24 @@ describe("postRouter tests", () => {
       content: "fourth content",
     };
 
-    const response = await request(app).post("/posts/new").send(testPost);
+    const response = await postNewPostRequest(testPost);
     const { statusCode, body } = response;
-    testPosts.push(testPost);
 
+    expect(statusCode).not.toBe(200);
     expect(statusCode).toBe(201);
-    expect(body).toEqual(expect.arrayContaining(testPosts));
+
+    expect(body).not.toContainEqual(expect.objectContaining(dummyData));
+    expect(body).toContainEqual(expect.objectContaining(testPost));
   });
 
   test("POST endpoint for invalid new user", async () => {
-    const response = await request(app).post("/posts/new").send({});
+    const response = await postNewPostRequest({});
     const { statusCode, body } = response;
 
+    expect(statusCode).not.toBe(300);
     expect(statusCode).toBe(400);
+
+    expect(body).not.toBeNull();
     expect(body).toHaveProperty("error");
   });
 });
