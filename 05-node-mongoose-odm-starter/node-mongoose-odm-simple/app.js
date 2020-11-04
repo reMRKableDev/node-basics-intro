d4126f5d4e76b3f01ebcf00a56d3742c3678502b
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-11-05 00:26:28 +0100

    :art: Create done, setup for RUD

--- 05-node-mongoose-odm-starter/node-mongoose-odm-simple/app.js
+++ 05-node-mongoose-odm-starter/node-mongoose-odm-simple/app.js
@@ -1,5 +1,21 @@
 const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
 
+// Model Schema
+const shoeSchema = new Schema(
+  {
+    name: String,
+    brand: String,
+    size: Number,
+    color: String,
+    isAvailable: Boolean,
+  },
+  { timestamps: true }
+);
+
+const Shoe = model("Shoes", shoeSchema);
+
+// Mongoose connection
 mongoose
   .connect("mongodb://localhost/mongoose-intro", {
     useCreateIndex: true,
@@ -11,4 +27,42 @@ mongoose
       `Successfully connected to Mongo! Database name: ${db.connections[0].name}`
     )
   )
-  .catch((error) => console.error(`Error when connecting to Mongo: ${error}`));
+  .catch((error) =>
+    console.error(`Error occurred while connecting to Mongo: ${error}`)
+  );
+
+const airMax = {
+  name: "Air Max 97",
+  brand: "Nike",
+  size: 44,
+  color: "Wolf Grey/Black/White/Red",
+  isAvailable: true,
+};
+
+/* CREATE */
+
+// .save() --> ran on instance method of a model.
+const nikeShoe = new Shoe(airMax);
+/* nikeShoe
+  .save()
+  .then((shoeSavedInDb) => console.log(`New shoe saved: ${shoeSavedInDb}`))
+  .catch((saveError) => console.error(`Error while saving: ${saveError}`)); */
+
+// .create() --> ran on the model itself, common way.
+Shoe.create(airMax)
+  .then((shoeSavedInDb) => console.log(`New shoe saved: ${shoeSavedInDb}`))
+  .catch((createError) => console.error(`Error while saving: ${createError}`));
+
+/* READ */
+
+/* UPDATE */
+
+/* DELETE */
+
+// Mongoose disconnection
+process.on("SIGINT", () => {
+  mongoose.connection.close(() => {
+    console.log("Mongo connection disconnected through app termination!");
+    process.exit(0);
+  });
+});
