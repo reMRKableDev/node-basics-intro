94b83a7766e0cde61ad9bc30673f83c6f5c17466
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-11-06 08:07:01 +0100

    :art: Included POST example

--- 04-node-express-api/node-omdb-api/app.js
+++ 04-node-express-api/node-omdb-api/app.js
@@ -1,41 +1,70 @@
+const hbs = require("hbs");
 const path = require("path");
 const axios = require("axios");
+const morgan = require("morgan");
 const express = require("express");
 const app = express();
 const PORT = 5000;
 require("dotenv").config();
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(__dirname + "/views/partials");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/', (req, res) => {
-    res.render('index');
-})
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
-app.get('/searchResults', (req, res) => {
-    const { movieName } = req.query;
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-    axios.get(`http://www.omdbapi.com/?apikey=${process.env.OMDB_API_Key}&s=${movieName}&page=10`)
-    .then( (OMDBResponse) => {
-       const {Search, totalResults} = OMDBResponse.data;
+app.get("/searchResults", (req, res) => {
+  const { movieName } = req.query;
 
-        res.render('search-results', { movieName, Search, totalResults });
+  axios
+    .get(
+      `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_Key}&s=${movieName}&page=10`
+    )
+    .then((OMDBResponse) => {
+      const { Search, totalResults } = OMDBResponse.data;
+
+      res.render("search-results", { movieName, Search, totalResults });
     })
-    .catch((error) => console.log(error))
-})
+    .catch((error) => console.log(error));
+});
 
-app.get('/movie/:movieId', (req, res) => {
-    const { movieId } = req.params;
+app.get("/movie/:movieId", (req, res) => {
+  const { movieId } = req.params;
 
-    axios.get(`http://www.omdbapi.com/?apikey=${process.env.OMDB_API_Key}&i=${movieId}`)
-    .then( (OMDBMovie) => {
-        const movie = OMDBMovie.data;
-        
-        res.render('movie', { movie });
+  axios
+    .get(
+      `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_Key}&i=${movieId}`
+    )
+    .then((OMDBMovie) => {
+      const movie = OMDBMovie.data;
+
+      res.render("movie", { movie });
     })
-    .catch((error) => console.log(error))
-})
+    .catch((error) => console.log(error));
+});
 
-app.listen(PORT, () =>{ console.log(`Server is running ${PORT}`)})
+app.post("/searchResults", (req, res) => {
+  const { movieName } = req.body;
+
+  axios
+    .get(
+      `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_Key}&s=${movieName}&page=10`
+    )
+    .then((OMDBResponse) => {
+      const { Search, totalResults } = OMDBResponse.data;
+
+      res.render("search-results", { movieName, Search, totalResults });
+    })
+    .catch((error) => console.log(error));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running ${PORT}`);
+});
