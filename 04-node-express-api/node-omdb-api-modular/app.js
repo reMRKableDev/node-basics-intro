0a2afe12ba61693b4d600e3c74c82a6a30da69e4
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-11-06 11:25:58 +0100

    :art: New changes

---
+++ 04-node-express-api/node-omdb-api-modular/app.js
@@ -0,0 +1,5 @@
+const hbs = require("hbs");
+const path = require("path");
+const axios = require("axios");
+const morgan = require("morgan");
+const express = require("express");
--- 04-node-express-api/node-omdb-api/app.js
+++ 04-node-express-api/node-omdb-api/app.js
@@ -1,19 +1,20 @@
-const hbs = require("hbs");
-const path = require("path");
-const axios = require("axios");
-const morgan = require("morgan");
-const express = require("express");
+const hbs = require("hbs"); // Views
+const path = require("path"); // joins two directories or files
+const axios = require("axios"); // make calls to external APIs
+const morgan = require("morgan"); // logger, show on the terminal which route was recently called
+const express = require("express"); // used to make express server + application
 const app = express();
 const PORT = 5000;
-require("dotenv").config();
+require("dotenv").config(); // Loads the values from .env file to process.env
 
 hbs.registerPartials(__dirname + "/views/partials");
 
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
 
+// Middleware
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); // get form data from the client to the server
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
@@ -21,11 +22,12 @@ app.get("/", (req, res) => {
 });
 
 app.get("/searchResults", (req, res) => {
+  // req.query --> Query String
   const { movieName } = req.query;
 
   axios
     .get(
-      `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_Key}&s=${movieName}&page=10`
+      `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_Key}&s=${movieName}&page=1-2`
     )
     .then((OMDBResponse) => {
       const { Search, totalResults } = OMDBResponse.data;
@@ -36,6 +38,7 @@ app.get("/searchResults", (req, res) => {
 });
 
 app.get("/movie/:movieId", (req, res) => {
+  console.log(req.params);
   const { movieId } = req.params;
 
   axios
@@ -43,6 +46,7 @@ app.get("/movie/:movieId", (req, res) => {
       `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_Key}&i=${movieId}`
     )
     .then((OMDBMovie) => {
+      console.log(OMDBMovie.data);
       const movie = OMDBMovie.data;
 
       res.render("movie", { movie });
@@ -53,7 +57,7 @@ app.get("/movie/:movieId", (req, res) => {
 app.post("/searchResults", (req, res) => {
   const { movieName } = req.body;
 
-  axios
+    axios
     .get(
       `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_Key}&s=${movieName}&page=10`
     )
