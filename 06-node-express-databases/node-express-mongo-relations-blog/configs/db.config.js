39649726e97f2cb228da4795a5ceba5d32e8adcc
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-11-09 23:19:46 +0100

    :recycle: Introduced configs fodler

---
+++ 06-node-express-databases/node-express-mongo-relations-blog/configs/db.config.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+exports.connectDB = mongoose
+  .connect("mongodb://localhost/node-express-mongo-relations-blog", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
+  });
