51710e099a8ca601c2a39233a62a5ddf336f71af
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-11-10 22:59:55 +0100

    :heavy_plus_sign: re-introduced debug

--- 07-node-express-authentication/node-auth-user-registration/models/User.model.js
+++ 07-node-express-authentication/node-auth-user-registration/models/User.model.js
@@ -1,25 +1,29 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
   {
     username: {
       type: String,
       trim: true,
-      required: [true, 'Username is required.'],
-      unique: true
+      required: [true, "Username is required."],
+      unique: true,
     },
     email: {
       type: String,
-      required: [true, 'Email is required.'],
+      required: [true, "Email is required."],
       unique: true,
       lowercase: true,
-      trim: true
-    }
+      trim: true,
+    },
     // add password property here
+    passwordHash: {
+      type: String,
+      required: [true, "Password is required."],
+    },
   },
   {
-    timestamps: true
+    timestamps: true,
   }
 );
 
-module.exports = model('User', userSchema);
+module.exports = model("User", userSchema);
