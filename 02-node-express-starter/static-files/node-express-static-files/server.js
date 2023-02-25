a300d97bddf423f7de44bcc7ddb6ba824884462f
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2023-02-25 19:19:37 +0100

    :bulb: j

--- 02-node-express-starter/static-files/node-express-static-files/server.js
+++ 02-node-express-starter/static-files/node-express-static-files/server.js
@@ -1,6 +1,25 @@
 require("dotenv").config();
 
+/**
+ * @name app
+ * @type {Object}
+ *
+ * @description The express app
+ */
 const app = require("./app");
-const { PORT } = process.env; //  ---> const PORT = process.env.PORT;
 
+/**
+ * @name PORT
+ * @type {Number}
+ *
+ * @description The port on which the server will run on. The port is set in the .env file
+ */
+const { PORT } = process.env;
+
+/**
+ * @name app.listen
+ * @type {Function}
+ * @param {Number} port - The port on which the server will run on
+ * @param {Function} callback - The callback function to be executed when the server is running
+ */
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
